c6331660cd7e888880654f7070a61e384e7df88a
Mahatur <97416515+Mahatur@users.noreply.github.com>
2025-02-26 10:38:31 +0700

    Update and rename togglenet2 to togglenet2.sh

--- togglenet2.sh
+++ togglenet2.sh
@@ -1,5 +1,5 @@
-INTERFACE="enp3s0"
-INTERFACE2="enp4s0"
+INTERFACE="internet"
+INTERFACE2="internet"
 RATE="100000kbps"
 BURST="1kbit"
 LATENCY="500ms"
@@ -11,10 +11,10 @@ if [ -z "$CURRENT_QDISC" ] && [ -z "$CURRENT_QDISC2" ]; then
     sudo tc qdisc add dev $INTERFACE root tbf rate $RATE burst $BURST latency $LATENCY
     sudo tc qdisc add dev $INTERFACE2 root tbf rate $RATE burst $BURST latency $LATENCY
     echo "Freeze mode activated on both interfaces."
-    paplay /home/mahatur/Utility/Selffreeze.ogg &
+    paplay /home/user/Utility/Selffreeze.ogg &
     sleep 1
 elif [ -n "$CURRENT_QDISC" ] && [ -n "$CURRENT_QDISC2" ]; then
-    paplay /home/mahatur/Utility/Selfthaw.ogg &
+    paplay /home/user/Utility/Selfthaw.ogg &
     sudo tc qdisc del dev $INTERFACE root
     sudo tc qdisc del dev $INTERFACE2 root
     echo "Normal mode restored."
