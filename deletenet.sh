599e1c1cac8f5498ff9b4bdda03805fa9e83332c
Mahatur <97416515+Mahatur@users.noreply.github.com>
2025-02-28 22:26:45 +0700

    Update deletenet.sh

--- deletenet.sh
+++ deletenet.sh
@@ -3,8 +3,8 @@ INTERFACE="internet"
 INTERFACE2="internet"
 
 sudo tc qdisc del dev $INTERFACE root
-sudo tc qdisc del dev $INTERFACE root
+sudo tc qdisc del dev $INTERFACE2 root
 sudo wondershaper clear $INTERFACE
-sudo wondershaper clear $INTERFACE
+sudo wondershaper clear $INTERFACE2
 paplay /home/user/Utility/Door.ogg &
 echo "All traffic controls cleared!"
