d744ad13ada606edbbacc3752523f318c4014d2e
Mahatur <97416515+Mahatur@users.noreply.github.com>
2025-07-28 20:10:10 +0700

    Update togglenet3.sh

--- togglenet3.sh
+++ togglenet3.sh
@@ -4,18 +4,18 @@ INTERFACE="internet"
 INTERFACE2="internet"
 DOWNLOAD="10"
 UPLOAD="0"
-FLAG_FILE="/home/user/Utility/wondershaper_active"
+FLAG_FILE="/home/user/RIT/wondershaper_active"
 
 if [ ! -f "$FLAG_FILE" ]; then
     sudo wondershaper $INTERFACE $DOWNLOAD $UPLOAD
     sudo wondershaper $INTERFACE2 $DOWNLOAD $UPLOAD
     touch "$FLAG_FILE"
     echo "You-freeze mode activated."
-    paplay /home/user/Utility/Otherfreeze.ogg &
+    paplay /home/user/RIT/Otherfreeze.ogg &
 else
     sudo wondershaper clear $INTERFACE
     sudo wondershaper clear $INTERFACE2
     rm "$FLAG_FILE"
     echo "Normal mode restored."
-    paplay /home/user/Utility/Otherthaw.ogg &
+    paplay /home/user/RIT/Otherthaw.ogg &
 fi
