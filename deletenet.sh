6a08db847b283672caa6d0f5816eb26d5a35384a
Mahatur <97416515+Mahatur@users.noreply.github.com>
2025-07-28 20:08:30 +0700

    Update deletenet.sh

--- deletenet.sh
+++ deletenet.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 INTERFACE="internet"
 INTERFACE2="internet"
-rm /home/user/Utility/wondershaper_active
+rm /home/user/RIT/wondershaper_active
 sudo tc qdisc del dev $INTERFACE root
 sudo tc qdisc del dev $INTERFACE2 root
 sudo wondershaper clear $INTERFACE
 sudo wondershaper clear $INTERFACE2
-paplay /home/user/Utility/Chest.ogg &
+paplay /home/user/RIT/Chest.ogg &
 echo "All traffic controls cleared!"
