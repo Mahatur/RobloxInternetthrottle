1a69ab014d9eb02eb8ea1ccd687b5bc61960bb1d
Mahatur <97416515+Mahatur@users.noreply.github.com>
2025-07-28 20:13:38 +0700

    Update togglenet.sh

--- togglenet.sh
+++ togglenet.sh
@@ -10,10 +10,10 @@ if [ -z "$CURRENT_QDISC" ] && [ -z "$CURRENT_QDISC2" ]; then
     sudo tc qdisc add dev $INTERFACE root tbf rate $SPEED burst 4kbit latency 500ms
     sudo tc qdisc add dev $INTERFACE2 root tbf rate $SPEED burst 4kbit latency 500ms
     echo "Speed limit added to both interfaces."
-    paplay /home/user/RIT/Lag.ogg &
+    paplay /home/user/RIT/Sounds/Lag.ogg &
     sleep 1
 elif [ -n "$CURRENT_QDISC" ] && [ -n "$CURRENT_QDISC2" ]; then
-    paplay /home/user/RIT/Lag1.ogg &
+    paplay /home/user/RIT/Sounds/Lag1.ogg &
     sleep 3
     sudo tc qdisc del dev $INTERFACE root
     sudo tc qdisc del dev $INTERFACE2 root
